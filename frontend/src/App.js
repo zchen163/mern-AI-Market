cecc7e9e787ab173a3cefa20f3c745f64b8f8171
zchen163 <chen.zhang06@gmail.com>
2023-06-29 07:59:07 -0500

    create sign-in api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>AI Market</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
